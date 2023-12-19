97eb52046ac187781b37cfa03d9a9dd3a24edfc7
yuzbamaria <54204342+yuzbamaria@users.noreply.github.com>
2023-12-19 00:08:05 +0000

    added code to fetchData function to pupulate #today and #forecast

--- script.js
+++ script.js
@@ -4,6 +4,10 @@ let queryURL;
 let searchBtn = $('#search-button');
 let cityName;
 let cityBtn;
+let todayWeather = $('#today');
+let todayTitle = $('<h2>');
+let weatherForecast = $('#forecast');
+
 
 // Create an empty array to store city names from user input in global scope
 let citiesArray = [];
@@ -46,8 +50,25 @@ function renderInput(city) {
 function storeCityList() {
     localStorage.setItem('city-names', JSON.stringify(citiesArray));
 }
-// {city name}&appid={API key}
 
+// date 15/9/2022
+// icon
+// temp: in celcius
+// wind: 
+// humidity:
+
+// When a user views the current weather conditions for that city they are presented with:
+    // The city name
+    // The date
+    // An icon representation of weather conditions
+    // The temperature
+    // The humidity
+    // The wind speed
+// When a user view future weather conditions for that city they are presented with a 5-day forecast that displays:
+    // The date
+    // An icon representation of weather conditions
+    // The temperature
+    // The humidity
 
 function fetchData() {
     //  Build the API query URL based on the user input value
@@ -58,12 +79,73 @@ function fetchData() {
     .then(response => response.json())
     .then(function (data) {
         console.log(data);
-        
+
+        let date = data.list[0].dt_txt;
+        todayTitle.text(cityName + " (" + date + ")");
+        let temp = data.list[0].main.temp;
+        let p1 = $('<p>').text("Temperature: " + temp);
+        let wind = data.list[0].wind.speed;
+        let p2 = $('<p>').text("Wind: " + wind + " KPH");
+        let humidity = data.list[0].main.humidity;
+        let p3 = $('<p>').text("Humidity: " + humidity + "%");
+        todayWeather.append(todayTitle, p1, p2, p3);
+
+        // Populate #forecast
+        // Create a card container
+        let results = data.list;
+        for (let i=0; i<results.length; i++) {
+            const cardContainer = $('<div>');
+            cardContainer.addClass('card', 'mb-3');
+
+            // Create a card body
+            const cardBody = $('<div>');
+            cardBody.addClass('card-body');
+
+            // Create a card title (h5)
+            const cardTitle = $('<h5>');
+            cardTitle.addClass('card-title');
+            cardTitle.text(results[i].dt_txt);
+            console.log(cardTitle);
+
+            // Create a card text (p)
+            const cardP1 = $('<p>');
+            let tempForecast = results[i].main.temp;
+            cardP1.addClass('card-text1');
+            cardP1.text("Temperature: " + tempForecast);
+
+            const cardP2 = $('<p>');
+            let windForecast = results[i].wind.speed;;
+            cardP2.addClass('card-text2');
+            cardP2.text("Wind: " + windForecast + " KPH");
+
+            const cardP3 = $('<p>');
+            let humidityForecast = results[i].main.humidity;;
+            cardP3.addClass('card-text2');
+            cardP3.text("Humidity: " + humidityForecast + "%");
+
+            // Append title and text to card body
+            cardBody.append(cardTitle);
+            cardBody.append(cardP1, cardP2, cardP3);
+            console.log(cardBody);
+            
+            // Append card body to card container
+            cardContainer.append(cardBody);
+
+            // Append the card to the forecast section
+            weatherForecast.append(cardContainer);
+
+        }
     })
     .catch(function (error) {
             console.error('Error:', error);
     });
    
+}
+
+function renderToday() {
+}
+
+function renderForecast() {
 }
 
 
